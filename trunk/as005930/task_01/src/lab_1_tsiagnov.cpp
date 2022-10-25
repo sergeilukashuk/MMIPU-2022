4d25b8ecdf3d681ac75e83ab0c98ee4c1e0c92b4
Евгений Цыганов <57139874+Ts1ganoV@users.noreply.github.com>
2022-10-25 14:13:45 +0300

    Lab1 (#28)

---
+++ trunk/as005930/task_01/src/lab_1_tsiagnov.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+
+
+using namespace std;
+
+const double a1 = 1.0;
+const double a2 = 1.0;
+const double b1 = 1;
+const double b2 = 0.0005;
+const double c = 2.0;
+const double d = 2.0;
+typedef const double c_tmp;
+
+double LinearFunction (double temperature, double input_warm) {
+    return a1 * temperature + b1 * input_warm;
+}
+
+double NonLinearFunction(c_tmp& temperature, double& input_warm, int& time) {
+    return a2 * temperature - b2 * pow((temperature-1), 2) + c * input_warm + d * sin(input_warm);
+}
+
+int main()
+{
+    ofstream fout;
+    fout.open("rezult.txt");
+    setlocale(LC_ALL, "RUS");
+    int time;
+    double temperature =0;
+    double input_warm;
+    cout << "Введите входящую температуру (t): ";
+    cin>>temperature;
+    cout << "Введите входящее тепло (U): ";
+    cin >> input_warm;
+    cout << "Введите time: ";
+    cin>>time;
+
+    cout << "Линейная функция: "<<endl;
+
+    for (int i= 0; i < time; i++) {
+        temperature = LinearFunction(temperature, input_warm);
+        
+        cout << temperature<<endl;
+        fout << temperature << endl;
+        
+    }
+    fout << endl;
+    cout << "Нелинейная функция: " << endl;
+
+    for (int l = 0; l < time; l++) {
+   
+        temperature = NonLinearFunction(temperature, input_warm, time);
+        cout << temperature << endl;
+        fout << temperature << endl;
+}
+    fout.close();
+    return 0;
+}
+
