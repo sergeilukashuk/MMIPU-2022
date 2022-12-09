546ea8a4a47b8683369bdb66483670931f405a3c
sergeilukashuk <appleprime12@yandex.by>
2022-12-10 01:28:31 +0300

    update

--- trunk/as005920/task_02/src/main.cpp
+++ trunk/as005920/task_02/src/main.cpp
@@ -1,88 +1,45 @@
 #include <iostream>
 #include <cmath>
-#include <algorithm>
-/**
- * @mainpage
- * @brief ПИД-регулятор
- * @author Лукашук Сергей Игоревич
- */
+using namespace std;
 
-/**
- * @class Regulator
- * @brief Абстрактный класс моделей вычисления
- */
 class Regulator{
 public:
-    virtual double regulator(double in_temp, double in_warm) = 0;
+    virtual double regulator(double in_temp, double in_warm,double _a, double _b, double _c, double _d) = 0;
 };
 
-/**
- * @class nolinemodel
- * @brief Класс нелинейной модели
- * @details Является подклассом Model
- */
-class nolinemodel: public Regulator{
+class NoLineModel: public Regulator{
 private:
-    /**
-     * @brief Константы нелинейной модели
-     *
-     * @param   temp        Текущая температура
-     * @param   a           Параметр А
-     * @param   b           Параметр B
-     * @param   c           Параметр С
-     * @param   d           Параметр D
-     */
-    const double a = 1, b = 0.0043, c = 0.515, d =0.515;
+
     double c_warm = 0;
     double c_temp = 0;
 public:
-    /**
-     * @brief   Метод вычисления температуры для нелинейной температуры
-     * @param   in_warm    Выходное тепло
-     * @return  double
-     */
-    double regulator(double in_temp, double in_warm) {
+    double regulator(double in_temp, double in_warm, double _a, double _b, double _c, double _d) {
         double temp;
-        temp = a * in_temp - b * pow(c_temp, 2) + c * in_warm + d * sin(c_warm);
+        temp = _a * in_temp - _b * pow(c_temp, 2) + _c * in_warm + _d * sin(c_warm);
         c_warm = in_warm;
         c_temp = in_temp;
         return temp;
     }
 };
-/**
- * @class PID_controller
- * @brief Абстрактный класс пид-контроллера
- */
+
 class PID_controller{
 private:
-    /**
-     * @brief controller
-     *
-     * @param   T   Параметр Т
-     * @param   T0  Параметр Т0
-     * @param   Td  Параметр Td
-     * @param   K   Параметр k
-     */
-    double u = 0, Td = 45, T =11, T0 = 14, k = 0.1;
+    double _u;
 public:
-    double controller(double e, double e1, double e2){
-        double q0 = k*(1+(Td/T0));
-        double q1 = -1*k*(1+2*(Td/T0)-(T0/T));
-        double q2 = k * (Td/T0);
-        u += q0*e + q1*e1 + q2*e2;
-        return u;
+    double controller(double& e, double& e1, double& e2,double& _Td, double& _T, double& _T0, double& _k){
+        double q0 = _k*(1+(_Td/_T0));
+        double q1 = -1*_k*(1+2*(_Td/_T0)-(_T0/_T));
+        double q2 = _k * (_Td/_T0);
+        _u += q0*e + q1*e1 + q2*e2;
+        return _u;
     }
-    void Reset_U(){
-        u = 0;
-    }
-    double PID_contr(double w, double y0, Regulator* model){
+    double PID_contr(double& w, double& y0, Regulator* model,double& _a, double& _b, double& _c, double& _d,double& _Td, double& _T, double& _T0, double& _k){
         double errorPrev = 0, error2Prev = 0, y = y0;
         for (int i = 0; i <100; i++) {
             double error, u;
             error = w - y;
-            u = controller(error, errorPrev, error2Prev);
-            y = model->regulator(y0, u);
-            std::cout << "E = " << error << ", Y = " << y << ", U = " << u << std::endl;
+            _u = controller(error, errorPrev, error2Prev,_Td,_T,_T0,_k);
+            y = model->regulator(y0, _u,_a,_b,_c,_d);
             error2Prev = errorPrev;
             errorPrev = error;
         }
@@ -90,10 +47,34 @@ public:
 };
 
 int main(){
-    double w = 80, y = 10;
-    int number;
+
+    double w , y;
+    double a , b , c , d ;
+    double u,Td, T, T0, k;
+    cout<<"Input w: ";
+    cin>>w;
+    cout<<"Input y: ";
+    cin>>y;
+    cout<<"Input a: ";
+    cin>>a;
+    cout<<"Input b: ";
+    cin>>b;
+    cout<<"Input c: ";
+    cin>>c;
+    cout<<"Input d: ";
+    cin>>d;
+    cout<<"Input u: ";
+    cin>>u;
+    cout<<"Input Td: ";
+    cin>>Td;
+    cout<<"Input T: ";
+    cin>>T;
+    cout<<"Input T0: ";
+    cin>>T0;
+    cout<<"Input k: ";
+    cin>>k;
     PID_controller* pid_contr = new PID_controller;
-    nolinemodel* nlModel = new nolinemodel;
-    pid_contr->PID_contr(w,y,nlModel);
+    NoLineModel* nlModel = new NoLineModel;
+    pid_contr->PID_contr(w,y,nlModel,a,b,c,d,Td,T,T0,k);
     return 0;
 }
